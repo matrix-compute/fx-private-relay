ae973fe127d6182c582abad233566090ddf54993
Vanessa Prema <vprema@mozilla.com>
2025-02-02 17:37:28 -0500

    MPP-4024: change types lint error

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,11 +1,16 @@
 declare namespace JSX {
-  interface Element extends React.ReactElement {}
   interface IntrinsicElements {
     // By default TypeScript will mark custom elements as invalid;
     // this defines <firefox-private-relay-addon> (and -data) as equivalent
     // to a <div>, allowing us to e.g. set data attributes on it.
     // See https://github.com/Microsoft/TypeScript/issues/4648#issuecomment-137845601.
-    "firefox-private-relay-addon": JSX.IntrinsicElements.div;
-    "firefox-private-relay-addon-data": JSX.IntrinsicElements.div;
+    "firefox-private-relay-addon": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+    "firefox-private-relay-addon-data": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
   }
 }
